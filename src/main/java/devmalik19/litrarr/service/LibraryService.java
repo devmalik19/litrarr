5ac0adbee1f44b54776bd03c7cd0876978bf2bf7
Deepak Malik <deepak.malik2@boeing.com>
2026-08-25 17:30:29 +0200

    Added natural sorting & Search on key press

--- src/main/java/devmalik19/litrarr/service/LibraryService.java
+++ src/main/java/devmalik19/litrarr/service/LibraryService.java
@@ -6,6 +6,7 @@ import devmalik19.litrarr.constants.*;
 import devmalik19.litrarr.data.dao.Item;
 import devmalik19.litrarr.data.dao.Library;
 import devmalik19.litrarr.data.dao.LibraryFilter;
+import devmalik19.litrarr.helper.SortingHelper;
 import devmalik19.litrarr.repository.ItemRepository;
 import devmalik19.litrarr.repository.LibraryFilterRepository;
 import devmalik19.litrarr.repository.LibraryRepository;
@@ -203,10 +204,10 @@ public class LibraryService
 	{
 		switch (sort)
 		{
-			case "name" -> library.sort(Comparator.comparing(Library::getName, String.CASE_INSENSITIVE_ORDER));
+			case "name" -> library.sort(Comparator.comparing(Library::getName, SortingHelper.naturalOrder()));
 			case "directory" -> library.sort(Comparator.comparing(
 				lib -> Path.of(lib.getPath()).getParent().getFileName().toString(),
-				String.CASE_INSENSITIVE_ORDER
+				SortingHelper.naturalOrder()
 			));
 		}
 	}
